2bdd662a4056d58efe50bdff38fab445e6288907
Volodymyr Boychyshyn <qsunnv@gmail.com>
2022-05-19 12:57:42 +0300

    added hw1

---
+++ hw1-variables-types/app.js
@@ -0,0 +1,50 @@
+const apples = 15.678;
+const oranges = 90.2345;
+const bananas = 123.965;
+
+const maxPrice = Math.max(apples, oranges, bananas);
+console.log(maxPrice);
+
+const minPrice = Math.min(apples, oranges, bananas);
+console.log(minPrice);
+
+const sum = apples + oranges + bananas;
+console.log(sum);
+
+const roundedSum = Math.floor(apples) + Math.floor(oranges) + Math.floor(bananas);
+console.log(roundedSum);
+
+const roundedToHundredthSum = Math.round(roundedSum / 100) * 100;
+console.log(roundedToHundredthSum);
+
+const isSumEven = !(roundedSum % 2);
+console.log(isSumEven);
+
+const changeFor500 = 500 - sum;
+console.log(changeFor500);
+
+const average = +(sum / 3).toFixed(2);
+console.log(average);
+
+const randomSale = Math.round(Math.random() * (99 - 1) + 1);
+console.log(randomSale);
+
+const sumWithSale = +(sum - (sum * randomSale / 100)).toFixed(2);
+console.log(sumWithSale);
+
+const netProfit = +(sumWithSale - (sum / 2)).toFixed(2);
+console.log(netProfit);
+
+document.writeln(`
+<p>Максимальна ціна: ${maxPrice}</p>
+<p>Мінімальна ціна: ${minPrice}</p>
+<p>Сума товарів: ${sum}</p>
+<p>Округлена сума товарів: ${roundedSum}</p>
+<p>Округлена до сотень сума товарів: ${roundedToHundredthSum}</p>
+<p>Чи є парним числом: ${isSumEven}</p>
+<p>Решта з 500: ${changeFor500}</p>
+<p>Середнє значення цін: ${average}</p>
+<p>Випадкова знижка: ${randomSale}%</p>
+<p>Ціна зі знижкою: ${sumWithSale}</p>
+<p>Чистий прибуток: ${netProfit}</p>
+`);
