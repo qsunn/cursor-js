c2d39610ae391a795cd6a93b2cf4607327ae15bc
Volodymyr Boychyshyn <qsunnv@gmail.com>
2022-06-20 17:09:03 +0300

    added click EL

--- hw10-events/main.js
+++ hw10-events/main.js
@@ -50,3 +50,46 @@ document.addEventListener('keypress', e => {
             return;
     }
 });
+
+document.querySelector('.container').addEventListener('click', e => {
+    switch (e.target.innerText) {
+        case 'Q':
+            if (!sound1.paused) sound1.currentTime = 0;
+            sound1.play();
+            break;
+        case 'W':
+            if (!sound2.paused) sound2.currentTime = 0;
+            sound2.play();
+            break;
+        case 'E':
+            if (!sound3.paused) sound3.currentTime = 0;
+            sound3.play();
+            break;
+        case 'A':
+            if (!sound4.paused) sound4.currentTime = 0;
+            sound4.play();
+            break;
+        case 'S':
+            if (!sound5.paused) sound5.currentTime = 0;
+            sound5.play();
+            break;
+        case 'D':
+            if (!sound6.paused) sound6.currentTime = 0;
+            sound6.play();
+            break;
+        case 'Z':
+            if (!sound7.paused) sound7.currentTime = 0;
+            sound7.play();
+            break;
+        case 'X':
+            if (!sound8.paused) sound8.currentTime = 0;
+            sound8.play();
+            break;
+        case 'C':
+            if (!sound9.paused) sound9.currentTime = 0;
+            sound9.play();
+            break;
+        default:
+            return;
+    }
+});
