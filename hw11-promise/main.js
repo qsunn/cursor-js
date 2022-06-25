c466f1f4e3161a1250d81bae20726b848165decb
Volodymyr Boychyshyn <qsunnv@gmail.com>
2022-06-25 13:36:28 +0300

    reworked for to while

--- hw11-promise/main.js
+++ hw11-promise/main.js
@@ -1,9 +1,12 @@
 async function getRandomChinese(length) {
     let result = '';
-    for (let i = 0; i < length; i++) {
+    while (length > 0) {
         result += String.fromCharCode((Date.now() + '').slice(-5));
-        await new Promise(res => setTimeout(() => res(), 50));
+        await new Promise(res => setTimeout(() => {
+            length--;
+            res(), 50;
+        }));
     };
     console.log(result);
 };
-getRandomChinese(4);
+getRandomChinese(8);
